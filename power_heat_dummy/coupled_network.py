da64a7bb6bca456c488ce9eebf5628f12a6b2da1
anthie <anthi.p.4@gmail.com>
2022-05-26 01:22:21 +0300

    Update elements to match given networks

--- power_heat_dummy/coupled_network.py
+++ power_heat_dummy/coupled_network.py
@@ -17,22 +17,22 @@ add_net_to_multinet(multinet, net_heat, 'heat')
 
 
 # create elements corresponding to conversion units:
-p2h_id_el = pandapower.create_load(net_power, bus=8, p_mw=0.3, name="power to heat consumption")
-p2h_id_heat = pandapipes.create_source(net_heat, junction=1, mdot_kg_per_s=1, name="power to gas feed in")
+p2h_id_el = pandapower.create_load(net_power, bus=8, p_mw=0.07, name="power to heat consumption")
+p2h_id_heat = pandapipes.create_source(net_heat, junction=1, mdot_kg_per_s=0, name="power to heat feed in")
 
-h2p_id_heat = pandapipes.create_sink(net_heat, junction=1, mdot_kg_per_s=1, name="gas to power consumption")
-h2p_id_el = pandapower.create_sgen(net_power, bus=8, p_mw=0.5, name="fuel cell feed in")
+h2p_id_heat = pandapipes.create_sink(net_heat, junction=1, mdot_kg_per_s=1, name="heat to power consumption")
+h2p_id_el = pandapower.create_sgen(net_power, bus=8, p_mw=0, name="fuel cell feed in")
 
 
 # create coupling controllers:
-p2h_ctrl = P2HControlMultiEnergy(multinet, p2h_id_el, p2h_id_heat, efficiency=1,
-                                 name_power_net="power", name_gas_net="heat")
+p2h_ctrl = P2HControlMultiEnergy(multinet, p2h_id_el, p2h_id_heat, cop_factor=4, temp_diff=50,
+                                 name_power_net="power", name_heat_net="heat")
 
 h2p_ctrl = H2PControlMultiEnergy(multinet, h2p_id_el, h2p_id_heat, efficiency=1,
-                                 name_power_net="power", name_gas_net="heat")
+                                 name_power_net="power", name_heat_net="heat")
 
 # run simulation:
 run_control(multinet)
 
-print(net_heat.source.loc[p2h_id_heat, 'mdot_kg_per_s'])
+print(net_heat.source.loc[p2h_id_heat, 'mdot_kg_per_s_out'])
 print(net_power.sgen.loc[h2p_id_el, 'p_mw'])
