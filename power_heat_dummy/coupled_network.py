487c5a74bbecbdf3a60f74f156f2fc5b0edadc70
anthie <anthi.p.4@gmail.com>
2022-06-08 01:00:18 +0300

    Add atributes to run_control()

--- power_heat_dummy/coupled_network.py
+++ power_heat_dummy/coupled_network.py
@@ -22,13 +22,9 @@ p2h_id_heat = pandapipes.create_source(net_heat, junction=1, mdot_kg_per_s=0, na
 p2h_ctrl = P2HControlMultiEnergy(multinet, p2h_id_el, p2h_id_heat, cop_factor=4, out_temp=373.15,
                                  name_power_net="power", name_heat_net="heat")
 
-control_variables = {
-    "run": 'net=net_heat, stop_condition="tol", iter=100, friction_model="colebrook", mode="all", transient=False, nonlinear_method="automatic", tol_res=1e-4)'
-}
-
-
 # run simulation:
-run_control(multinet, ctrl_variables=control_variables)
+run_control(multinet, stop_condition="tol", iter=100, friction_model="colebrook",
+                    mode="all", transient=False, nonlinear_method="automatic", tol_res=1e-4)
 
 print(net_heat.source.loc[p2h_id_heat, 'mdot_kg_per_s'])
 
@@ -36,5 +32,4 @@ buses = list([i for i in range(1, 7)]) + [8, 9]
 for bus in buses:
     print(str(net_power.bus.name[bus]) + ': ' + str(round(net_power.res_bus.vm_pu[bus], 7)))
 
-print(net_heat.res_junction["t_k"])
-# net_heat.res_pipe.to_csv("pipes_example.csv")
+print(net_heat.res_junction)
