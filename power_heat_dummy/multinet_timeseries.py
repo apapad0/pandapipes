be725345c51f4b2cfba9715a1549b137eb22168e
anthie <anthi.p.4@gmail.com>
2022-06-20 01:34:00 +0300

    Timeseries for multinet

---
+++ power_heat_dummy/multinet_timeseries.py
@@ -0,0 +1,61 @@
+from os.path import join, dirname
+
+import pandapower
+import pandas
+from pandapower.timeseries import DFData, OutputWriter
+
+import pandapipes
+from coupled_network import multinet, p2h_id_el, p2h_id_heat, pipeflow_attributes
+from pandapipes.multinet.timeseries.run_time_series_multinet import run_timeseries
+from pandapipes.multinet.control.controller.multinet_control import coupled_p2h_const_control
+
+
+def create_datasource(csv_name):
+    df = pandas.read_csv(csv_name)
+    ds = DFData(df)
+    return ds
+
+
+def create_output_writers(multinet, time_steps=None):
+    nets = multinet["nets"]
+    ows = dict()
+    for key_net in nets.keys():
+        ows[key_net] = {}
+        if isinstance(nets[key_net], pandapower.pandapowerNet):
+            log_variables = [('res_bus', 'vm_pu'),
+                             ('res_line', 'loading_percent'),
+                             ('res_line', 'i_ka'),
+                             ('res_bus', 'p_mw'),
+                             ('res_bus', 'q_mvar'),
+                             ('res_load', 'p_mw'),
+                             ('res_load', 'q_mvar')]
+            ow = OutputWriter(nets[key_net], time_steps=time_steps,
+                              log_variables=log_variables,
+                              output_path=join(dirname('__file__'), 'timeseries', 'results', 'power'),
+                              output_file_type=".csv", csv_separator=",")
+            ows[key_net] = ow
+        elif isinstance(nets[key_net], pandapipes.pandapipesNet):
+            log_variables = [('res_sink', 'mdot_kg_per_s'),
+                             ('res_source', 'mdot_kg_per_s'),
+                             ('res_ext_grid', 'mdot_kg_per_s'),
+                             ('res_pipe', 'v_mean_m_per_s'),
+                             ('res_junction', 'p_bar'),
+                             ('res_junction', 't_k')]
+            ow = OutputWriter(nets[key_net], time_steps=time_steps,
+                              log_variables=log_variables,
+                              output_path=join(dirname('__file__'), 'timeseries', 'results', 'heat'),
+                              output_file_type=".csv", csv_separator=",")
+            ows[key_net] = ow
+        else:
+            raise AttributeError("Could not create an output writer for nets of kind " + str(key_net))
+    return ows
+
+
+if __name__ == "__main__":
+    timesteps = range(7)
+    ds = create_datasource("loads.csv")
+    ows = create_output_writers(multinet, timesteps)
+    coupled_p2h_const_control(multinet, p2h_id_el, p2h_id_heat, cop_factor=4, out_temp=373.15,
+                              name_power_net="power", name_heat_net="heat", profile_name=["load"], data_source=ds)
+
+    run_timeseries(multinet, time_steps=timesteps, output_writers=ows, **pipeflow_attributes)
